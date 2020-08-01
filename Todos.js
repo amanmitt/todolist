0cf3b44d7a1da51dcaafb5ddf2a8015f3d310dfe
xsuvw <69079923+xsuvw@users.noreply.github.com>
2020-08-01 18:55:04 +0530

    Add files via upload

---
+++ Todos.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Todos = ({todos, deleteTodo}) => {
+
+    const todolist = todos.length ? (
+        todos.map(todo => {
+            return (
+                <div className = "collection-item" key = {todo.id}>
+                    <span onClick = {() => {deleteTodo(todo.id)}}>
+                        {todo.content}
+                    </span>
+                </div>
+            )
+        })
+    ) : (
+        <p className = "center"> 
+            You are done for the day!
+        </p>
+    )
+
+    return (
+        <div className = "todos collection">
+            {todolist}
+        </div>
+    )
+}
+
+
+export default Todos
